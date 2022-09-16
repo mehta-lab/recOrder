2bf550644099d958be5501a1f5ff99906b738016
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2022-09-16 14:49:20 -0700

    format .postproc

--- recOrder/postproc/post_processing.py
+++ recOrder/postproc/post_processing.py
@@ -5,7 +5,9 @@ from colorspacious import cspace_convert
 from matplotlib.colors import hsv_to_rgb
 
 
-def generic_hsv_overlay(H, S, V, H_scale=None, S_scale=None, V_scale=None, mode='2D'):
+def generic_hsv_overlay(
+    H, S, V, H_scale=None, S_scale=None, V_scale=None, mode="2D"
+):
     """
     Generates a generic HSV overlay in either 2D or 3D
 
@@ -27,9 +29,10 @@ def generic_hsv_overlay(H, S, V, H_scale=None, S_scale=None, V_scale=None, mode=
 
     if H.shape != S.shape or H.shape != S.shape or S.shape != V.shape:
         raise ValueError(
-            f'Channel shapes do not match: {H.shape} vs. {S.shape} vs. {V.shape}')
+            f"Channel shapes do not match: {H.shape} vs. {S.shape} vs. {V.shape}"
+        )
 
-    if mode == '3D':
+    if mode == "3D":
         overlay_final = np.zeros((H.shape[0], H.shape[1], H.shape[2], 3))
         slices = H.shape[0]
     else:
@@ -47,10 +50,10 @@ def generic_hsv_overlay(H, S, V, H_scale=None, S_scale=None, V_scale=None, mode=
         hsv = np.transpose(np.stack([H_, S_, V_]), (1, 2, 0))
         overlay_final[i] = hsv_to_rgb(hsv)
 
-    return overlay_final[0] if mode == '2D' else overlay_final
+    return overlay_final[0] if mode == "2D" else overlay_final
 
 
-def ret_ori_overlay(retardance, orientation, scale, mode='2D', cmap='JCh'):
+def ret_ori_overlay(retardance, orientation, scale, mode="2D", cmap="JCh"):
     """
     This function will create an overlay of retardance and orientation with two different colormap options.
     HSV is the standard Hue, Saturation, Value colormap while JCh is a similar colormap but is perceptually uniform.
@@ -76,23 +79,30 @@ def ret_ori_overlay(retardance, orientation, scale, mode='2D', cmap='JCh'):
 
     if retardance.shape != orientation.shape:
         raise ValueError(
-            f'Retardance and Orientation shapes do not match: {retardance.shape} vs. {orientation.shape}')
+            f"Retardance and Orientation shapes do not match: {retardance.shape} vs. {orientation.shape}"
+        )
 
-    if mode == '3D':
-        overlay_final = np.zeros((retardance.shape[0], retardance.shape[1], retardance.shape[2], 3))
+    if mode == "3D":
+        overlay_final = np.zeros(
+            (retardance.shape[0], retardance.shape[1], retardance.shape[2], 3)
+        )
         slices = retardance.shape[0]
     else:
-        overlay_final = np.zeros((1, retardance.shape[-2], retardance.shape[-1], 3))
+        overlay_final = np.zeros(
+            (1, retardance.shape[-2], retardance.shape[-1], 3)
+        )
         orientation = np.expand_dims(orientation, axis=0)
         retardance = np.expand_dims(retardance, axis=0)
         slices = 1
 
     for i in range(slices):
         ret_ = np.interp(retardance[i], ret_scale, scale)
-        ori_binned = np.round(orientation[i] / 180 * levels + 0.5) / levels - 1 / levels
+        ori_binned = (
+            np.round(orientation[i] / 180 * levels + 0.5) / levels - 1 / levels
+        )
         ori_ = np.interp(ori_binned, (0, 1), (0, 360))
 
-        if cmap == 'JCh':
+        if cmap == "JCh":
             J = ret_
             C = np.ones_like(J) * 60
             C[retardance[i] < noise_level] = 0
@@ -105,15 +115,24 @@ def ret_ori_overlay(retardance, orientation, scale, mode='2D', cmap='JCh'):
             JCh_rgb[JCh_rgb > 1] = 1
 
             overlay_final[i] = JCh_rgb
-        elif cmap == 'HSV':
-            I_hsv = np.transpose(np.stack([ori_binned, np.ones_like(ori_binned),
-                                           np.minimum(1, ret_ / np.max(ret_))]), (1, 2, 0))
+        elif cmap == "HSV":
+            I_hsv = np.transpose(
+                np.stack(
+                    [
+                        ori_binned,
+                        np.ones_like(ori_binned),
+                        np.minimum(1, ret_ / np.max(ret_)),
+                    ]
+                ),
+                (1, 2, 0),
+            )
             overlay_final[i] = hsv_to_rgb(I_hsv)
 
         else:
-            raise ValueError(f'Colormap {cmap} not understood')
+            raise ValueError(f"Colormap {cmap} not understood")
 
-    return overlay_final[0] if mode == '2D' else overlay_final
+    return overlay_final[0] if mode == "2D" else overlay_final
+
 
 def post_proc_denoise(data_volume, params):
     """
@@ -133,10 +152,14 @@ def post_proc_denoise(data_volume, params):
     data_volume_denoised = np.copy(data_volume)
 
     if len(data_volume) == 1:
-        data_volume_denoised = wavelet_softThreshold(data_volume[0], 'db8', params[1], params[2])
+        data_volume_denoised = wavelet_softThreshold(
+            data_volume[0], "db8", params[1], params[2]
+        )
     else:
         for z in range(len(data_volume)):
-            data_volume_denoised[z, :, :] = wavelet_softThreshold(data_volume[z], 'db8', params[1], params[2])
+            data_volume_denoised[z, :, :] = wavelet_softThreshold(
+                data_volume[z], "db8", params[1], params[2]
+            )
 
     return data_volume_denoised
 
