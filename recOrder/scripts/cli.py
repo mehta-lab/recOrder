f2e73a05e5e21159ad0aa0b25a5d0a9a3c183e49
Talon Chandler <talonchandler@gmail.com>
2023-03-29 15:23:16 -0700

    Rewrite `recorder view` for use with v0.4 ome-zarr

--- recOrder/scripts/cli.py
+++ recOrder/scripts/cli.py
@@ -1,8 +1,141 @@
 import click
 import napari
 import numpy as np
-from iohub.reader import print_info
-from iohub import read_micromanager
+from iohub.reader import print_info, _infer_format
+from iohub import read_micromanager, open_ome_zarr
+from iohub.reader_base import ReaderBase
+from iohub.ngff import NGFFNode, Plate, Position, TiledPosition
+
+
+def _get_reader(filename):
+    fmt, extra_info = _infer_format(filename)
+    if fmt == "omezarr" and extra_info == "0.4":
+        reader = open_ome_zarr(filename, mode="r")
+    else:
+        reader = read_micromanager(filename, data_type=fmt)
+    return reader
+
+
+def _build_complete_position_list(reader):
+    if isinstance(reader, ReaderBase):
+        positions = range(reader.get_num_positions())
+    elif isinstance(reader, Plate):
+        positions = [x[0] for x in list(reader.positions())]
+    elif isinstance(reader, Position) or isinstance(reader, TiledPosition):
+        positions = ["0"]
+    else:
+        raise (
+            NotImplementedError,
+            f"`recOrder view` does not support {type(reader)}.",
+        )
+    return positions
+
+
+def _build_array_list(reader, positions, layers):
+    arrays = []
+    if isinstance(reader, ReaderBase):
+        positions = [int(x) for x in positions]
+
+        # Positions as layers
+        if layers == "position" or layers == "p":
+            for position in positions:
+                arrays.append(reader.get_zarr(position))
+
+        # Channels as layers
+        elif layers == "channel" or layers == "c":
+            print(
+                """WARNING: for sending channels to layers is more expensive than 
+sending positions to layers. Try loading a small number of positions."""
+            )
+
+            ptzyx = (len(positions),) + (reader.shape[0],) + reader.shape[2:]
+            for channel in range(int(reader.channels)):
+                temp_data = np.zeros(ptzyx)
+                for k, position in enumerate(positions):
+                    temp_data[k] = reader.get_array(position)[:, channel, ...]
+                arrays.append(temp_data)
+
+    elif isinstance(reader, NGFFNode):
+
+        # Positions as layers
+        if layers == "position" or layers == "p":
+            for position in positions:
+                arrays.append(reader[position].data)
+
+        # Channels as layers
+        elif layers == "channel" or layers == "c":
+            T, C, Z, Y, X = reader[positions[0]].data.shape
+            ptzyx = (len(positions),) + (T, Z, Y, X)
+            for c in range(C):
+                temp_data = np.zeros(ptzyx)
+                for k, position in enumerate(positions):
+                    temp_data[k] = reader[position].data[:, c, ...]
+                arrays.append(temp_data)
+
+    return arrays
+
+
+def _build_name_lists(reader, positions, layers):
+    layer_names = []
+    slice_names = []
+
+    # Build list of position_names
+    if isinstance(reader, ReaderBase):
+        positions = [int(x) for x in positions]
+        position_names = []
+        for position in positions:
+            try:
+                pos_name = reader.stage_positions[position]["Label"]
+            except:
+                pos_name = "Pos" + str(position)
+            position_names.append(pos_name)
+    elif isinstance(reader, NGFFNode):
+        position_names = positions
+
+    # Assign position and channel names to layers or slices
+    if layers == "position" or layers == "p":
+        layer_names = position_names
+        slice_names = reader.channel_names
+    elif layers == "channel" or layers == "c":
+        layer_names = reader.channel_names
+        slice_names = position_names
+
+    return layer_names, slice_names
+
+
+def _create_napari_viewer(arrays, layers, layer_names, slice_names):
+    # Create and populate napari viewer
+    v = napari.Viewer()
+
+    # Add arrays
+    for i, array in enumerate(arrays):
+        v.add_image(array, name=layer_names[i])
+        v.layers[-1].reset_contrast_limits()
+
+    # Cosmetic labelling
+    v.text_overlay.visible = True
+    v.text_overlay.color = "green"
+
+    if layers == "channel" or layers == "c":
+        v.dims.axis_labels = ("P", "T", "Z", "Y", "X")
+
+        def text_overlay():
+            v.text_overlay.text = (
+                f"Position: {slice_names[v.dims.current_step[0]]}"
+            )
+
+    elif layers == "position" or layers == "p":
+        v.dims.axis_labels = ("T", "C", "Z", "Y", "X")
+
+        def text_overlay():
+            v.text_overlay.text = (
+                f"Channel: {slice_names[v.dims.current_step[1]]}"
+            )
+
+    v.dims.events.current_step.connect(text_overlay)
+    text_overlay()
+
+    napari.run()
 
 
 @click.group()
@@ -20,7 +153,10 @@ def cli():
     "-p",
     default=None,
     multiple=True,
-    help="Integer positions to open. Accepts multiple positions: -p 0 -p 1 -p 10.",
+    help="""For ome-zarr v0.4 datasets, position name string e.g. \"-p 0/0/0\"
+for the 0th row, 0th column, and 0th fov. For other datasets, integer positions
+e.g. \"-p 0\" for the 0th position. Accepts multiple positions e.g. \"-p 0/0/0
+-p 0/1/10\".""",
 )
 @click.option(
     "--layers",
@@ -34,47 +170,12 @@ def view(filename, positions=None, layers=None):
     click.echo(f"Reading file:\t {filename}")
     print_info(filename)
 
-    reader = read_micromanager(filename)
+    reader = _get_reader(filename)
 
-    if positions == ():  # If empty, open all positions
-        positions = range(reader.get_num_positions())
-    positions = [int(x) for x in positions]
+    if positions == ():  # If no positions specified, open all positions
+        positions = _build_complete_position_list(reader)
 
-    v = napari.Viewer()
-    v.text_overlay.visible = True
-    v.text_overlay.color = "green"
-    if layers == "position" or layers == "p":
-        for position in positions:
-            try:
-                name = reader.stage_positions[position]["Label"]
-            except:
-                name = "Pos" + str(position)
-            v.add_image(reader.get_zarr(position), name=name)
-            v.layers[-1].reset_contrast_limits()
-        v.dims.axis_labels = ("T", "C", "Z", "Y", "X")
+    arrays = _build_array_list(reader, positions, layers)
+    layer_names, slice_names = _build_name_lists(reader, positions, layers)
 
-        # Overlay the channel name
-        def text_overlay():
-            v.text_overlay.text = (
-                f"Channel: {reader.channel_names[v.dims.current_step[1]]}"
-            )
-
-        v.dims.events.current_step.connect(text_overlay)
-        text_overlay()
-
-    elif layers == "channel" or layers == "c":
-        print(
-            "WARNING: sending channels to layers is more expensive than sending positions to layers. "
-            "Try loading a small number of positions."
-        )
-
-        ptzyx = (len(positions),) + (reader.shape[0],) + reader.shape[2:]
-        for channel in range(int(reader.channels)):
-            temp_data = np.zeros(ptzyx)
-            for k, position in enumerate(positions):
-                temp_data[k] = reader.get_array(position)[:, channel, ...]
-            v.add_image(temp_data, name=reader.channel_names[channel])
-            v.layers[-1].reset_contrast_limits()
-        v.dims.axis_labels = ("P", "T", "Z", "Y", "X")
-
-    napari.run()
+    _create_napari_viewer(arrays, layers, layer_names, slice_names)
