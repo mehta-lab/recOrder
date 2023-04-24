74b3557c58829aa01184a470546ce3618af208b1
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2023-04-24 14:55:06 -0700

    do not import napari

--- recOrder/tests/widget_tests/test_dock_widget.py
+++ recOrder/tests/widget_tests/test_dock_widget.py
@@ -1,8 +1,9 @@
 from recOrder.plugin.main_widget import MainWidget
-import napari
+# import napari
+from napari.viewer import ViewerModel
 
 
 def test_dock_widget(make_napari_viewer):
-    viewer: napari.Viewer = make_napari_viewer()
+    viewer: ViewerModel = make_napari_viewer()
     viewer.window.add_dock_widget(MainWidget(viewer))
     assert "recOrder" in list(viewer._window._dock_widgets.keys())[0]
