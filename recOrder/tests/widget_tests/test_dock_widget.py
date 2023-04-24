4d0bb61f8682e6fb29a0fdc0a378555b09f2e614
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2023-04-24 14:17:13 -0700

    Revert "delete the widget"
    This reverts commit 3594e424b2172d5732de66d2d78b97be0f1faa85.

--- recOrder/tests/widget_tests/test_dock_widget.py
+++ recOrder/tests/widget_tests/test_dock_widget.py
@@ -2,10 +2,7 @@ from recOrder.plugin.main_widget import MainWidget
 import napari
 
 
-def test_dock_widget(make_napari_viewer, qtbot):
+def test_dock_widget(make_napari_viewer):
     viewer: napari.Viewer = make_napari_viewer()
-    widget = MainWidget(viewer)
-    viewer.window.add_dock_widget(widget)
+    viewer.window.add_dock_widget(MainWidget(viewer))
     assert "recOrder" in list(viewer._window._dock_widgets.keys())[0]
-    widget.deleteLater()
-    qtbot.wait(50)
