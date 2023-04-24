ccc8dad2de5c0cb4566800dab334010706d42024
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2023-04-24 12:15:28 -0700

    test if the napari viewer fixture works will revert later

--- recOrder/tests/widget_tests/test_dock_widget.py
+++ recOrder/tests/widget_tests/test_dock_widget.py
@@ -4,8 +4,4 @@ import napari
 
 def test_dock_widget(make_napari_viewer, qtbot):
     viewer: napari.Viewer = make_napari_viewer()
-    widget = MainWidget(viewer)
-    viewer.window.add_dock_widget(widget)
-    assert "recOrder" in list(viewer._window._dock_widgets.keys())[0]
-    widget.deleteLater()
-    qtbot.wait(50)
+    assert viewer
