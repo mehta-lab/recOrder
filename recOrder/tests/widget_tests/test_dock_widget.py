3594e424b2172d5732de66d2d78b97be0f1faa85
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2023-04-24 11:37:31 -0700

    delete the widget see https://github.com/napari/napari/issues/3948

--- recOrder/tests/widget_tests/test_dock_widget.py
+++ recOrder/tests/widget_tests/test_dock_widget.py
@@ -2,7 +2,10 @@ from recOrder.plugin.main_widget import MainWidget
 import napari
 
 
-def test_dock_widget(make_napari_viewer):
+def test_dock_widget(make_napari_viewer, qtbot):
     viewer: napari.Viewer = make_napari_viewer()
-    viewer.window.add_dock_widget(MainWidget(viewer))
+    widget = MainWidget(viewer)
+    viewer.window.add_dock_widget(widget)
     assert "recOrder" in list(viewer._window._dock_widgets.keys())[0]
+    widget.deleteLater()
+    qtbot.wait(50)
