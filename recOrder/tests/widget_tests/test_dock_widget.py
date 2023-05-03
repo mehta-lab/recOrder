dde94fa3f1e0bd5bc1bff28f76394e1ace74d412
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2023-05-03 13:35:47 -0700

    skip test for linux CI runners

--- recOrder/tests/widget_tests/test_dock_widget.py
+++ recOrder/tests/widget_tests/test_dock_widget.py
@@ -1,8 +1,14 @@
-from recOrder.plugin.main_widget import MainWidget
-# import napari
+import sys
+import os
+
+import pytest
 from napari.viewer import ViewerModel
 
+from recOrder.plugin.main_widget import MainWidget
 
+
+# skip this for GitHub actions Linux runner
+@pytest.mark.skipif("linux" in sys.platform and os.environ.get("CI"))
 def test_dock_widget(make_napari_viewer):
     viewer: ViewerModel = make_napari_viewer()
     viewer.window.add_dock_widget(MainWidget(viewer))
