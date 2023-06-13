d844477d94bf78b860aaa6dd5ddff36aea495567
Clinton Huynh <clinton.huynh@macbook-pro.czbiohub.org>
2023-06-12 17:28:33 -0700

    added tests for compute-transfer-function and separated function logic

--- recOrder/cli/compute_transfer_function.py
+++ recOrder/cli/compute_transfer_function.py
@@ -34,76 +34,10 @@ def compute_transfer_function_cli(config_path, output_path):
 
     # Pass settings to appropriate calculate_transfer_function and save
     if settings.universal_settings.reconstruct_birefringence:
-        echo_headline(
-            "Generating birefringence transfer function with settings:"
-        )
-        echo_settings(settings.birefringence_transfer_function_settings)
-
-        # Calculate transfer functions
-        intensity_to_stokes_matrix = (
-            inplane_anisotropic_thin_pol3d.calculate_transfer_function(
-                **settings.birefringence_transfer_function_settings.dict()
-            )
-        )
-        # Save
-        dataset[
-            "intensity_to_stokes_matrix"
-        ] = intensity_to_stokes_matrix.cpu().numpy()[None, None, None, ...]
+        dataset = generate_birefringence(settings, dataset)
 
     if settings.universal_settings.reconstruct_phase:
-        echo_headline("Generating phase transfer function with settings:")
-        echo_settings(settings.phase_transfer_function_settings)
-
-        if settings.universal_settings.reconstruction_dimension == 2:
-            # Convert zyx_shape and z_pixel_size into yx_shape and z_position_list
-            settings_dict = settings.phase_transfer_function_settings.dict()
-            z_shape, y_shape, x_shape = settings_dict["zyx_shape"]
-            settings_dict["yx_shape"] = [y_shape, x_shape]
-            settings_dict["z_position_list"] = list(
-                -(np.arange(z_shape) - z_shape // 2)
-                * settings_dict["z_pixel_size"]
-            )
-
-            # Remove unused parameters
-            settings_dict.pop("zyx_shape")
-            settings_dict.pop("z_pixel_size")
-            settings_dict.pop("z_padding")
-
-            # Calculate transfer functions
-            (
-                absorption_transfer_function,
-                phase_transfer_function,
-            ) = isotropic_thin_3d.calculate_transfer_function(
-                **settings_dict,
-                wavelength_illumination=settings.universal_settings.wavelength_illumination,
-            )
-
-            # Save
-            dataset[
-                "absorption_transfer_function"
-            ] = absorption_transfer_function.cpu().numpy()[None, None, ...]
-            dataset[
-                "phase_transfer_function"
-            ] = phase_transfer_function.cpu().numpy()[None, None, ...]
-
-        elif settings.universal_settings.reconstruction_dimension == 3:
-            # Calculate transfer functions
-            (
-                real_potential_transfer_function,
-                imaginary_potential_transfer_function,
-            ) = phase_thick_3d.calculate_transfer_function(
-                **settings.phase_transfer_function_settings.dict(),
-                wavelength_illumination=settings.universal_settings.wavelength_illumination,
-            )
-            # Save
-            dataset[
-                "real_potential_transfer_function"
-            ] = real_potential_transfer_function.cpu().numpy()[None, None, ...]
-            dataset[
-                "imaginary_potential_transfer_function"
-            ] = imaginary_potential_transfer_function.cpu().numpy()[
-                None, None, ...
-            ]
+        datatset = generate_phase(settings, dataset)
 
     # Write settings to metadata
     dataset.zattrs["transfer_function_settings"] = settings.dict()
@@ -114,6 +48,83 @@ def compute_transfer_function_cli(config_path, output_path):
     echo_headline(
         f"Recreate this transfer function with:\n>> recorder compute-transfer-function {config_path} -o {output_path}"
     )
+
+def generate_birefringence(settings, dataset):
+    echo_headline(
+        "Generating birefringence transfer function with settings:"
+    )
+    echo_settings(settings.birefringence_transfer_function_settings)
+
+    # Calculate transfer functions
+    intensity_to_stokes_matrix = (
+        inplane_anisotropic_thin_pol3d.calculate_transfer_function(
+            **settings.birefringence_transfer_function_settings.dict()
+        )
+    )
+    # Save
+    dataset[
+        "intensity_to_stokes_matrix"
+    ] = intensity_to_stokes_matrix.cpu().numpy()[None, None, None, ...]    
+
+    return dataset
+
+
+def generate_phase(settings, dataset):
+    echo_headline("Generating phase transfer function with settings:")
+    echo_settings(settings.phase_transfer_function_settings)
+
+    if settings.universal_settings.reconstruction_dimension == 2:
+        # Convert zyx_shape and z_pixel_size into yx_shape and z_position_list
+        settings_dict = settings.phase_transfer_function_settings.dict()
+        z_shape, y_shape, x_shape = settings_dict["zyx_shape"]
+        settings_dict["yx_shape"] = [y_shape, x_shape]
+        settings_dict["z_position_list"] = list(
+            -(np.arange(z_shape) - z_shape // 2)
+            * settings_dict["z_pixel_size"]
+        )
+
+        # Remove unused parameters
+        settings_dict.pop("zyx_shape")
+        settings_dict.pop("z_pixel_size")
+        settings_dict.pop("z_padding")
+
+        # Calculate transfer functions
+        (
+            absorption_transfer_function,
+            phase_transfer_function,
+        ) = isotropic_thin_3d.calculate_transfer_function(
+            **settings_dict,
+            wavelength_illumination=settings.universal_settings.wavelength_illumination,
+        )
+
+        # Save
+        dataset[
+            "absorption_transfer_function"
+        ] = absorption_transfer_function.cpu().numpy()[None, None, ...]
+        dataset[
+            "phase_transfer_function"
+        ] = phase_transfer_function.cpu().numpy()[None, None, ...]
+
+    elif settings.universal_settings.reconstruction_dimension == 3:
+        # Calculate transfer functions
+        (
+            real_potential_transfer_function,
+            imaginary_potential_transfer_function,
+        ) = phase_thick_3d.calculate_transfer_function(
+            **settings.phase_transfer_function_settings.dict(),
+            wavelength_illumination=settings.universal_settings.wavelength_illumination,
+        )
+        # Save
+        dataset[
+            "real_potential_transfer_function"
+        ] = real_potential_transfer_function.cpu().numpy()[None, None, ...]
+        dataset[
+            "imaginary_potential_transfer_function"
+        ] = imaginary_potential_transfer_function.cpu().numpy()[
+            None, None, ...
+        ]
+
+    return dataset
 
 
 @click.command()
--- recOrder/tests/cli_tests/test_cli.py
+++ recOrder/tests/cli_tests/test_cli.py
@@ -1,6 +1,8 @@
 from recOrder.cli.main import cli
 from click.testing import CliRunner
-
+from unittest.mock import patch, Mock
+from iohub.ngff import open_ome_zarr, Position
+from recOrder.cli.compute_transfer_function import generate_phase, generate_birefringence
 
 def test_main():
     runner = CliRunner()
@@ -8,3 +10,26 @@ def test_main():
 
     assert result.exit_code == 0
     assert "Toolkit" in result.output
+
+def test_compute_transfer():
+    runner = CliRunner()
+    result = runner.invoke(cli, "compute-transfer-function")
+
+    assert result.exit_code == 0
+    assert "Generating" in result.output
+
+def test_compute_transfer_blank():
+    runner = CliRunner()
+    for option in ("-c", "--config-path"):
+        cmd = "compute-transfer-function" + option
+        result = runner.invoke(cli, cmd)
+        assert result.exit_code == 2
+        assert "Error" in result.output
+
+def test_compute_transfer_output_file():
+    runner = CliRunner()
+    for option in ("-o", "--output-path"):
+        cmd = "compute-transfer-function " + option
+        result = runner.invoke(cli, cmd)
+        assert result.exit_code == 0
+        assert dir in result.output
