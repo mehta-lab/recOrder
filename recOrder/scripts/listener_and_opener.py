b2cedbfff4a90bb955989f6eeb5d4cdc99ed7444
Clinton Huynh <clinton.huynh@czbiohub.org>
2023-08-03 10:17:29 -0700

    documentation and comments

--- recOrder/scripts/listener_and_opener.py
+++ recOrder/scripts/listener_and_opener.py
@@ -25,7 +25,7 @@ def update_dimensions(
     acq_mode, curr_p, curr_t, curr_c, curr_z, p_max, t_max, c_max, z_max
 ):
     """
-    Helper function to update the dimensions of the Multi-Dimensional Acquisition (MDA),
+    Helper function to update the dimensions to follow the Multi-Dimensional Acquisition (MDA),
     based on the acquisition mode.
 
     Parameters
@@ -120,7 +120,7 @@ def update_dimensions(
 
 def update_layers(data_name_tuple):
     """
-    Updates the napari layers at the given layer name and with the given data.
+    Updates the napari viewer at the given layer name with the given data.
 
     Parameters
     ----------
@@ -129,7 +129,6 @@ def update_layers(data_name_tuple):
     """
     position_data: ImageArray or np.ndarray = data_name_tuple[0]
     layer_name: str = data_name_tuple[1]
-    print(position_data.shape)
     print(f"Updating napari layer {layer_name}")
     if layer_name in viewer.layers:
         viewer.layers[layer_name].data = position_data
@@ -137,44 +136,15 @@ def update_layers(data_name_tuple):
         viewer.add_image(position_data, name=layer_name)
 
 
-channel_names = ""
-
-
-# @thread_worker(connect={"yielded": update_layers})
-# def read_zarr(path_and_position_tuple):
-#     """
-#     Given a zarr path, position, read the zarr store at the current position
-#     and yield the position data and layer name to update_layers.
-
-#     Parameters
-#     ----------
-#     path_and_position_and_z_tuple : tuple(string, int, boolean)
-#         A tuple containing a zarr path as a string, current position int, and whether or not
-#         a z-stack is done.
-
-#     Yields
-#     ------
-#     tuple(numpy array, string)
-#     Yields the position data as a numpy array, and layer name as a string in a tuple.
-#     """
-#     path: str = path_and_position_tuple[0]
-#     curr_p: int = path_and_position_tuple[1]
-#     with open_ome_zarr(path, layout="fov", mode="r") as dataset:
-#         print(f"Getting data from position {curr_p}")
-#         dataset.print_tree()
-#         position_data = dataset[0]
-#         yield position_data, f"Position {curr_p}"
-
-
 @thread_worker(connect={"yielded": update_layers})
 def reconstruct_zarr(queue):
     """
-    Reconstructs the imaging data in the queue.
+    Reconstructs the imaging data in the queue and yields the data to update_layers.
 
     Parameters
     ----------
     queue: Queue
-        A FIFO queue that has information for the reconstructions to happen.
+        A FIFO queue that has information for the reconstruction to happen.
 
     Yields
     ------
@@ -200,6 +170,7 @@ def reconstruct_zarr(queue):
                 os.path.dirname(os.path.dirname(zarr_path)),
                 "birefringence.yml",
             )
+            # Creates a pydantic model of the configuration settings
             config_settings = _load_configuration_settings(config_path)
             config_settings.time_indices = curr_time
 
@@ -212,6 +183,7 @@ def reconstruct_zarr(queue):
                 os.path.dirname(zarr_path),
                 f"reconstruction{curr_position}.zarr",
             )
+            # Birefringence only
             if (config_settings.birefringence is not None) and (
                 not config_settings.phase
             ):
@@ -254,6 +226,7 @@ def reconstruct_zarr(queue):
                 )
                 yield zarr_array, f"Overlay{curr_position}"
                 queue.task_done()
+            # Phase only
             elif (config_settings.phase is not None) and (
                 not config_settings.birefringence
             ):
@@ -274,7 +247,6 @@ def reconstruct_zarr(queue):
                     dataset.print_tree()
                     position_data = dataset[0]
                     yield position_data, f"Position {curr_position}"
-
                 queue.task_done()
 
 
@@ -293,7 +265,7 @@ def initialize_transfer_function_call(zarr_path: str, curr_p: int):
     Returns
     -------
     string
-        The path of the transfer function.
+        The path of the computed transfer function.
     """
     tf_start_time = time.time()
     input_data_path = os.path.join(zarr_path, "0", str(curr_p), "0")
@@ -314,7 +286,6 @@ def initialize_transfer_function_call(zarr_path: str, curr_p: int):
     return transfer_function_path
 
 
-# Runs the MDA and converts the data to ome-zarr, yielding the zarr path to read_zarr
 @thread_worker(connect={"yielded": reconstruct_zarr})
 def mda_to_zarr():
     """
@@ -345,24 +316,24 @@ def mda_to_zarr():
     z_max = intended_dimensions.getZ() - 1
 
     max_images = (p_max + 1) * (t_max + 1) * (c_max + 1) * (z_max + 1)
+    # Command arguments
     zarr_path = sys.argv[1]
     reconstruction_type = sys.argv[2]
     # Only for OME TIFF
     storage = datastore.getStorage()
-    print(dir(storage))
     if save_mode == "MULTIPAGE_TIFF":
         reader_map = storage.getCoordsToReader()
     path = datastore.getSavePath()
     file_header = os.path.basename(path)
-    initialize = True
 
+    # Dictionary to convert the acqusition mode into a string format
     acq_dictionary = {0: "TPZC", 1: "TPCZ", 2: "PTZC", 3: "PTCZ"}
     sequence_settings = engine.getSequenceSettings()
     acq_mode = acq_dictionary[sequence_settings.acqOrderMode()]
     curr_p, curr_t, curr_c, curr_z, img_count = 0, 0, 0, 0, 0
     initialize = True
     initialize_transfer_function = True
-    new_page = True
+    first_page = True
     last_file = None
     queue = Queue()
     yield queue
@@ -401,7 +372,7 @@ def mda_to_zarr():
                     path, f"{file_header}_MMStack_Pos{curr_p}.ome.tif"
                 )
                 if last_file != curr_file:
-                    new_page = True
+                    first_page = True
                     last_file = curr_file
             # Wait for file to exist before reading
             while not os.path.exists(curr_file):
@@ -426,7 +397,6 @@ def mda_to_zarr():
                 ) as dataset:
                     for p in range(p_max + 1):
                         position = dataset.create_position("0", p, "0")
-                        # position.create_zeros
                         position.create_zeros(
                             name="0",
                             shape=(
@@ -449,13 +419,13 @@ def mda_to_zarr():
                     data = Dataset(path)
                 image = data.read_image(curr_c, curr_z, curr_t, curr_p)
             elif save_mode == "MULTIPAGE_TIFF":
-                # # Obtain the reader and get the offset for the current image
+                # Obtain the reader and get the offset for the current image
                 curr_reader = reader_map.get(required_coord)
                 offset = curr_reader.getCoordsToOffset().get(required_coord)
                 # Add an offset, based on if the image is the first of the file
-                if new_page:
+                if first_page:
                     offset += 210
-                    new_page = False
+                    first_page = False
                 else:
                     offset += 162
 
@@ -477,7 +447,7 @@ def mda_to_zarr():
                 f"Current p: {curr_p}\t Current t: {curr_t}\t Current c: {curr_c}\t Current z: {curr_z}"
             )
 
-            # Check if the z-stack is finished
+            # Monitoring zyx chunks for phase reconstruction
             if reconstruction_type == "phase":
                 z_done = False
                 if acq_mode == "TPCZ" or acq_mode == "PTCZ":
@@ -502,6 +472,7 @@ def mda_to_zarr():
                         curr_t,
                     )
                     queue.put(send_tuple)
+            # Monitoring czyx chunks for birefringence reconstruction
             elif reconstruction_type == "birefringence":
                 c_done = False
                 if curr_z == z_max and curr_c == c_max:
