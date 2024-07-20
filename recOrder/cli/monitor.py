2bc61a58c03aa5200a71980203dba782b8017d68
Talon Chandler <talonchandler@gmail.com>
2024-07-19 17:01:46 -0700

    monitor when more jobs than terminal lines

--- recOrder/cli/monitor.py
+++ recOrder/cli/monitor.py
@@ -1,47 +1,90 @@
 from pathlib import Path
 
 import time
+import numpy as np
+import shutil
 import submitit
 import sys
 
 
-def _clear_status(jobs):
-    for job in jobs:
-        sys.stdout.write("\033[F")  # Move cursor up
-        sys.stdout.write("\033[K")  # Clear line
+def _move_cursor_up(n_lines):
+    sys.stdout.write("\033[F" * n_lines)
 
 
-def _print_status(jobs, position_dirpaths, elapsed_list):
+def _print_status(jobs, position_dirpaths, elapsed_list, print_indices=None):
+
+    columns = [15, 30, 40, 50]
+
+    # header
+    sys.stdout.write(
+        "\033[K"  # clear line
+        "\033[96mID"  # cyan
+        f"\033[{columns[0]}G WELL "
+        f"\033[{columns[1]}G STATUS "
+        f"\033[{columns[2]}G NODE "
+        f"\033[{columns[2]}G ELAPSED\n"
+    )
+
+    if print_indices is None:
+        print_indices = range(len(jobs))
+
+    complete_count = 0
     for i, (job, position_dirpath) in enumerate(zip(jobs, position_dirpaths)):
+        try:
+            node_name = job.get_info()["NodeList"]  # slowest, so do this first
+        except:
+            node_name = "SUBMITTED"
+
         if job.state == "COMPLETED":
             color = "\033[32m"  # green
+            complete_count += 1
         elif job.state == "RUNNING":
             color = "\033[93m"  # yellow
             elapsed_list[i] += 1  # inexact timing
         else:
             color = "\033[91m"  # red
 
-        try:
-            node_name = job.get_info()["NodeList"]
-        except:
-            node_name = "SUBMITTED"
+        if i in print_indices:
+            sys.stdout.write(
+                f"\033[K"  # clear line
+                f"{color}{job.job_id}"
+                f"\033[{columns[0]}G {'/'.join(position_dirpath.parts[-3:])}"
+                f"\033[{columns[1]}G {job.state}"
+                f"\033[{columns[2]}G {node_name}"
+                f"\033[{columns[3]}G {elapsed_list[i]} s\n"
+            )
+    sys.stdout.flush()
+    print(
+        f"\033[32m{complete_count}/{len(jobs)} jobs complete. " 
+        "<ctrl+z> to move monitor to background. " 
+        "<ctrl+c> twice to cancel jobs."
+    )
 
-        sys.stdout.write(
-            f"{color}{job.job_id}"
-            f"\033[15G {'/'.join(position_dirpath.parts[-3:])}"
-            f"\033[30G {job.state}"
-            f"\033[40G {node_name}"
-            f"\033[50G {elapsed_list[i]} s\n"
-        )
-        sys.stdout.flush()
     return elapsed_list
 
 
-def _print_header():
-    sys.stdout.write(
-        "\033[96mID\033[15G WELL \033[30G STATUS \033[40G NODE \033[50G ELAPSED\n"
-    )
-    sys.stdout.flush()
+def _get_jobs_to_print(jobs, num_to_print):
+    job_indices_to_print = []
+
+    # if number of jobs is smaller than termanal size, print all
+    if len(jobs) <= num_to_print:
+        return list(range(len(jobs)))
+
+    # prioritize incomplete jobs
+    for i, job in enumerate(jobs):
+        if not job.done():
+            job_indices_to_print.append(i)
+        if len(job_indices_to_print) == num_to_print:
+            return job_indices_to_print
+
+    # fill in the rest with complete jobs
+    for i, job in enumerate(jobs):
+        job_indices_to_print.append(i)
+        if len(job_indices_to_print) == num_to_print:
+            return job_indices_to_print
+
+    # shouldn't reach here
+    return job_indices_to_print
 
 
 def monitor_jobs(jobs: list[submitit.Job], position_dirpaths: list[Path]):
@@ -54,33 +97,55 @@ def monitor_jobs(jobs: list[submitit.Job], position_dirpaths: list[Path]):
     position_dirpaths : list[Path]
         List of corresponding position paths
     """
+    NON_JOB_LINES = 3
+
     if not len(jobs) == len(position_dirpaths):
         raise ValueError(
             "The number of jobs and position_dirpaths should be the same."
         )
 
     elapsed_list = [0] * len(jobs)  # timer for each job
-    try:
-        _print_header()
+
+    # print all jobs once if terminal is too small
+    if shutil.get_terminal_size().lines - NON_JOB_LINES < len(jobs):
         _print_status(jobs, position_dirpaths, elapsed_list)
+
+    # main monitor loop
+    try:
         while not all(job.done() for job in jobs):
+            terminal_lines = shutil.get_terminal_size().lines
+            num_jobs_to_print = np.min(
+                [terminal_lines - NON_JOB_LINES, len(jobs)]
+            )
+
+            job_indices_to_print = _get_jobs_to_print(jobs, num_jobs_to_print)
+
+            elapsed_list = _print_status(
+                jobs,
+                position_dirpaths,
+                elapsed_list,
+                job_indices_to_print,
+            )
+
             time.sleep(1)
-            _clear_status(jobs)
-            elapsed_list = _print_status(jobs, position_dirpaths, elapsed_list)
+            _move_cursor_up(num_jobs_to_print + 2)
 
         # Print final status
         time.sleep(1)
-        _clear_status(jobs)
         _print_status(jobs, position_dirpaths, elapsed_list)
 
+    # cancel jobs if ctrl+c
     except KeyboardInterrupt:
         for job in jobs:
             job.cancel()
         print("All jobs cancelled.")
 
-    # Print STDOUT and STDERR for single-job runs
-    if len(jobs) == 1:
-        print("\033[32mSTDOUT")
-        print(jobs[0].stdout())
-        print("\033[91mSTDERR")
-        print(jobs[0].stderr())
+    # Print STDOUT and STDERR for first incomplete job
+    incomplete_count = 0
+    for job in jobs:
+        if not job.done():
+            if incomplete_count == 0:
+                print("\033[32mSTDOUT")
+                print(job.stdout())
+                print("\033[91mSTDERR")
+                print(job.stderr())
